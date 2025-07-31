0278f934e9290b3924aacba237c60348d84ced95
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-08-01 01:25:31 +0530

    chore: update error text to display user not qualified and round not enabled

--- internal/middlewares/round_check.go
+++ internal/middlewares/round_check.go
@@ -18,7 +18,7 @@ func CheckRound(next http.Handler) http.Handler {
 		roundVal, _ := strconv.Atoi(round)
 
 		if user.RoundQualified != int32(roundVal) {
-			httphelpers.WriteError(w, http.StatusForbidden, "round not enabled yet")
+			httphelpers.WriteError(w, http.StatusForbidden, "user not qualified or round not enabled")
 			return
 		}
 		next.ServeHTTP(w, r)
